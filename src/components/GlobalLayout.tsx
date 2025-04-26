b1dc0cb38a1ef928f3c5c527238ee3e0d6c1ed0a
Ali Mortazavi <alimortazavi.pr@gmail.com>
2025-04-26 17:33:46 +0330

    feat: add bg

--- src/components/GlobalLayout.tsx
+++ src/components/GlobalLayout.tsx
@@ -1,13 +1,19 @@
 import type { FC, ReactNode } from "react";
+import { StarsBackground } from "./animate-ui/backgrounds/stars";
 
 //Components
 
 //Tools
 
 const GlobalLayout: FC<{ children: ReactNode }> = ({ children }) => (
-  <div className="p-3 md:p-5 max-w-[1700px] mx-auto">
-    <div>{children}</div>
-  </div>
+  <>
+    <div className="-z-50 h-[100dvh] w-screen fixed left-0 top-0">
+    <StarsBackground className="w-full h-full" />
+    </div>
+    <div className="p-3 md:p-5 max-w-[1700px] mx-auto z-10">
+      <div>{children}</div>
+    </div>
+  </>
 );
 
 export default GlobalLayout;
