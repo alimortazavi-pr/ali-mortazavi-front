9b24ecbb1280c6fcf454558b20d071ea649263c3
Ali Mortazavi <alimortazavi.pr@gmail.com>
2025-08-19 22:04:15 +0330

    change years experience from 4+ to 5+ in HeroSection

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -50,7 +50,7 @@ export const HeroSection: FC = () => {
       <div className="grid grid-cols-12 gap-3 xl:flex-auto">
         <div className="col-span-12 md:col-span-4 lg:col-span-6 xl:col-span-4 h-32 md:h-40 lg:h-40 xl:h-full bg-teal-500/70 flex flex-col items-center justify-center rounded-2xl md:rounded-3xl ">
           <div className="text-gray-100 text-5xl md:text-6xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold">
-            <span>4+</span>
+            <span>5+</span>
           </div>
           <div className="text-sm md:text-lg lg:text-base xl:text-lg xl:mt-2 2xl:text-lg text-gray-100">
             <span>Years Experience</span>
